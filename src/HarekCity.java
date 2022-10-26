19035d49138447cf6d77db504feecbd6519fada2
Faoeltiarna <moriartystreet@gmail.com>
2022-10-26 18:42:14 +0300

    HarekCity HW 7/27

---
+++ src/HarekCity.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class HarekCity {
+    public String[] createEmptyNameArray() {
+        String[] result = new String[0];
+        return result;
+    }
+
+    public int[] createAgeArray(int age1, int age2, int age3, int age4) {
+        return new int[] {age1, age2, age3, age4};
+    }
+
+    public void fixNames(String[] names, String[] toReplace){
+        System.arraycopy(toReplace, 0, names, 1, 1);
+        System.arraycopy(toReplace, 1, names, 3, 1);
+        return;
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"mixa", "ukio", "barek", "krou", "fiha"};
+        String[] toReplace = new String[]{"lopr", "boki"};
+        new HarekCity().fixNames(names, toReplace);
+        System.out.println(Arrays.toString(names));
+    }
+}
